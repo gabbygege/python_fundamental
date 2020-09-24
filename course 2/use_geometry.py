39a02231b3e8465a436fbb43fc8621f7ea49e307
GEGE <gabbyfebriann@gmail.com>
2020-09-24 18:12:01 +0700

    menambah file dan code baru

---
+++ course 2/use_geometry.py
@@ -0,0 +1,8 @@
+from Package.persegipanjang import hitung_luas_persegi_panjang, info as info_persegipanjang
+from Package.segitiga import hitung_luas_segitiga, info as info_segitiga
+
+print(info_segitiga())
+print(f'hitung_luas_segitiga {hitung_luas_segitiga(10, 3)}')
+
+print(info_persegipanjang())
+print(f'hitung_luas_persegi_panjang {hitung_luas_persegi_panjang(10, 3)}')
